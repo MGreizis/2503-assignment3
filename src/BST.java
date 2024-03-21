eb62f2a61d0c1a1b95b7433ff244af732c42de12
Martins Greizis <grei0003@hz.nl>
2024-03-21 15:55:08 -0600

    yeah

--- src/BST.java
+++ src/BST.java
@@ -5,113 +5,113 @@ import java.util.Comparator;
 
 public class BST<T extends Comparable<T>> implements Iterable<T> {
 
-  private BSTNode<T> root;
-  private Comparator<T> comparator;
+   private BSTNode<T> root;
+   private Comparator<T> comparator;
 
-  public BST() {
-    // Create a new BST using the natural ordering of T.
-    this.root = null;
-    this.comparator = null;
-  }
+   public BST() {
+      // Create a new BST using the natural ordering of T.
+      this.root = null;
+      this.comparator = null;
+   }
 
-  public BST(Comparator<T> c) {
-    // Create a new BST using the ordering determined by c
-    this.root = null;
-    this.comparator = c;
-  }
+   public BST(Comparator<T> c) {
+      // Create a new BST using the ordering determined by c
+      this.root = null;
+      this.comparator = c;
+   }
 
-  public void insert(T data) {
-    root = insertRec(root, data);
-  }
+   public void insert(T data) {
+      root = insertRec(root, data);
+   }
 
-  private BSTNode<T> insertRec(BSTNode<T> node, T data) {
-    if (node == null) {
-      return new BSTNode<>(data);
-    }
-
-    if (compare(data, node.getData()) < 0) {
-      node.setLeft(insertRec(node.getLeft(), data));
-    } else if (compare(data, node.getData()) > 0) {
-      node.setRight(insertRec(node.getRight(), data));
-    }
-
-    return node;
-  }
-
-  public boolean contains(T data) {
-    return containsRec(root, data);
-  }
-
-  private boolean containsRec(BSTNode<T> node, T data) {
-    if (node == null) {
-      return false;
-    }
-
-    if (compare(data, node.getData()) == 0) {
-      return true;
-    }
-
-    if (compare(data, node.getData()) < 0) {
-      return containsRec(node.getLeft(), data);
-    } else {
-      return containsRec(node.getRight(), data);
-    }
-  }
-
-  private int compare(T data1, T data2) {
-    if (comparator != null) {
-      return comparator.compare(data1, data2);
-    } else {
-      return data1.compareTo(data2);
-    }
-  }
-
-  public int size() {
-    return 0;
-  }
-
-  public int height() {
-    return 0;
-  }
-
-  @Override
-  public Iterator<T> iterator() {
-    return new BSTIterator<>(root);
-  }
-
-  public T min() {
-    return null;
-  }
-
-  private static class BSTIterator<T extends Comparable<T>> implements Iterator<T> {
-    private Queue<T> queue;
-
-    public BSTIterator(BSTNode<T> root) {
-      this.queue = new LinkedList<>();
-      inOrderTraversal(root);
-    }
-
-    private void inOrderTraversal(BSTNode<T> node) {
-      if (node != null) {
-        inOrderTraversal(node.getLeft());
-        queue.add(node.getData());
-        inOrderTraversal(node.getRight());
+   private BSTNode<T> insertRec(BSTNode<T> node, T data) {
+      if (node == null) {
+         return new BSTNode<>(data);
       }
-    }
 
-    @Override
-    public boolean hasNext() {
-      return !queue.isEmpty();
-    }
+      if (compare(data, node.getData()) < 0) {
+         node.setLeft(insertRec(node.getLeft(), data));
+      } else if (compare(data, node.getData()) > 0) {
+         node.setRight(insertRec(node.getRight(), data));
+      }
 
-    @Override
-    public T next() {
-      return queue.poll();
-    }
+      return node;
+   }
 
-    @Override
-    public void remove() {
-      throw new UnsupportedOperationException();
-    }
-  }
+   public boolean contains(T data) {
+      return containsRec(root, data);
+   }
+
+   private boolean containsRec(BSTNode<T> node, T data) {
+      if (node == null) {
+         return false;
+      }
+
+      if (compare(data, node.getData()) == 0) {
+         return true;
+      }
+
+      if (compare(data, node.getData()) < 0) {
+         return containsRec(node.getLeft(), data);
+      } else {
+         return containsRec(node.getRight(), data);
+      }
+   }
+
+   private int compare(T data1, T data2) {
+      if (comparator != null) {
+         return comparator.compare(data1, data2);
+      } else {
+         return data1.compareTo(data2);
+      }
+   }
+
+   public int size() {
+      return 0;
+   }
+
+   public int height() {
+      return 0;
+   }
+
+   @Override
+   public Iterator<T> iterator() {
+      return new BSTIterator<>(root);
+   }
+
+   public T min() {
+      return null;
+   }
+
+   private static class BSTIterator<T extends Comparable<T>> implements Iterator<T> {
+      private Queue<T> queue;
+
+      public BSTIterator(BSTNode<T> root) {
+         this.queue = new LinkedList<>();
+         inOrderTraversal(root);
+      }
+
+      private void inOrderTraversal(BSTNode<T> node) {
+         if (node != null) {
+            inOrderTraversal(node.getLeft());
+            queue.add(node.getData());
+            inOrderTraversal(node.getRight());
+         }
+      }
+
+      @Override
+      public boolean hasNext() {
+         return !queue.isEmpty();
+      }
+
+      @Override
+      public T next() {
+         return queue.poll();
+      }
+
+      @Override
+      public void remove() {
+         throw new UnsupportedOperationException();
+      }
+   }
 }
--- src/Token.java
+++ src/Token.java
@@ -8,128 +8,128 @@ import java.util.Comparator;
 
 public class Token implements Comparable<Token> {
 
-  // The word!
-  private String word;
-  // How many times it has occurred
-  private int count;
+   // The word!
+   private String word;
+   // How many times it has occurred
+   private int count;
 
-  /**
-   * Create a word with string w and count 0.
-   */
-  public Token(String w) {
-    this.word = w;
-    this.count = 0;
-  }
+   /**
+    * Create a word with string w and count 0.
+    */
+   public Token(String w) {
+      this.word = w;
+      this.count = 0;
+   }
 
-  /**
-   * Comparator to sort from high to low frequency.
-   */
-  public static Comparator<Token> CompFreqDesc = new Comparator<Token>() {
+   /**
+    * Comparator to sort from high to low frequency.
+    */
+   public static Comparator<Token> CompFreqDesc = new Comparator<Token>() {
 
-    public int compare(Token w1, Token w2) {
+      public int compare(Token w1, Token w2) {
 
-      int f1 = w1.getCount();
-      int f2 = w2.getCount();
-      if (f2 - f1 == 0)
-        return w1.compareTo(w2);
-      else
-        return f2 - f1;
-    }
-  };
-
-  /**
-   * Comparator to sort from low to high frequency.
-   */
-  public static Comparator<Token> CompFreqAsc = new Comparator<Token>() {
-
-    public int compare(Token w1, Token w2) {
-
-      int f1 = w1.getCount();
-      int f2 = w2.getCount();
-
-      if (f1 - f2 == 0)
-        return w1.compareTo(w2);
-      else
-        return f1 - f2;
-    }
-  };
-
-  /**
-   * Comparator to sort from high to low word length.
-   */
-  public static Comparator<Token> CompLengthDesc = new Comparator<Token>() {
-
-    public int compare(Token w1, Token w2) {
-      int len1 = w1.getLength();
-      int len2 = w2.getLength();
-
-      if (len1 == len2) {
-        return w1.getWord().compareTo(w2.getWord());
+         int f1 = w1.getCount();
+         int f2 = w2.getCount();
+         if (f2 - f1 == 0)
+            return w1.compareTo(w2);
+         else
+            return f2 - f1;
       }
+   };
 
-      return len2 - len1;
-    }
-  };
+   /**
+    * Comparator to sort from low to high frequency.
+    */
+   public static Comparator<Token> CompFreqAsc = new Comparator<Token>() {
 
-  /**
-   * Return the string representing this word.
+      public int compare(Token w1, Token w2) {
+
+         int f1 = w1.getCount();
+         int f2 = w2.getCount();
+
+         if (f1 - f2 == 0)
+            return w1.compareTo(w2);
+         else
+            return f1 - f2;
+      }
+   };
+
+   /**
+    * Comparator to sort from high to low word length.
+    */
+   public static Comparator<Token> CompLengthDesc = new Comparator<Token>() {
+
+      public int compare(Token w1, Token w2) {
+         int len1 = w1.getLength();
+         int len2 = w2.getLength();
+
+         if (len1 == len2) {
+            return w1.getWord().compareTo(w2.getWord());
+         }
+
+         return len2 - len1;
+      }
+   };
+
+   /**
+    * Return the string representing this word.
+    */
+   public String getWord() {
+      return word;
+   }
+
+   /**
+    * Return the number of times this word has occurred.
+    */
+   public int getCount() {
+      return count;
+   }
+
+   /**
+    * Increment the counter by 1.
+    */
+   public void incrCount() {
+      count++;
+   }
+
+   /**
+    * Return the length of the word.
+    */
+   public int getLength() {
+      return getWord().length();
+   }
+
+   /**
+    * Return a string representation of this word.
+    */
+   public String toString() {
+      return getWord() + " : " + getLength() + " : " + getCount();
+   }
+
+   public int hashCode() {
+      return getWord().hashCode();
+   }
+
+   /** 
    */
-  public String getWord() {
-    return word;
-  }
+   public boolean equals(Object other) {
+      if (other == this)
+         return true;
+      if (other == null)
+         return false;
+      if (this.getClass() != other.getClass())
+         return false;
+      Token p = (Token) other;
+      return this.getWord().equals(p.getWord());
+   }
 
-  /**
-   * Return the number of times this word has occurred.
-   */
-  public int getCount() {
-    return count;
-  }
-
-  /**
-   * Increment the counter by 1.
-   */
-  public void incrCount() {
-    count++;
-  }
-
-  /**
-   * Return the length of the word.
-   */
-  public int getLength() {
-    return getWord().length();
-  }
-
-  /**
-   * Return a string representation of this word.
-   */
-  public String toString() {
-    return getWord() + " : " + getLength() + " : " + getCount();
-  }
-
-  public int hashCode() {
-    return getWord().hashCode();
-  }
-
-  /** 
-   */
-  public boolean equals(Object other) {
-    if (other == this)
-      return true;
-    if (other == null)
-      return false;
-    if (this.getClass() != other.getClass())
-      return false;
-    Token p = (Token) other;
-    return this.getWord().equals(p.getWord());
-  }
-
-  /**
-   * The natural ordering for Words: alphabetic.
-   */
-  public int compareTo(Token o) {
-    if (this.equals(o))
-      return 0;
-    else
-      return this.getWord().compareTo(o.getWord());
-  }
+   /**
+    * The natural ordering for Words: alphabetic.
+    */
+   public int compareTo(Token o) {
+      if (this.equals(o))
+         return 0;
+      else
+         return this.getWord().compareTo(o.getWord());
+   }
 }
